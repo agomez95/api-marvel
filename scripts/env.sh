e698aa17b70b680595f4edafe0bebd951f9e3814
Adrian Gomez V <sandmancity14@gmail.com>
2023-05-12 13:01:43 -0500

    actualizacion de la API url

--- scripts/env.sh
+++ scripts/env.sh
@@ -4,4 +4,4 @@ export PORT=3000
 
 export PUBLICKEY=72179ad2e635d66ba47d6e0ef468ae0d
 export PRIVATEKEY=e67d9098e82e9e6b2092402cf835026257ebd696
-export API="https://developer.marvel.com/v1/public/"
+export API="https://gateway.marvel.com/v1/public/"
